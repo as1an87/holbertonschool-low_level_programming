027a1142d5150deec6cf262b1ce55067c19fa58f
as1an87 <as1anoff87@gmail.com>
2024-02-24 21:20:00 +0000

    sign

---
+++ functions_nested_loops/5-sign.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * _isalpha - Checks if a character is lowercase or uppercase
+ * @c: character to check
+ * Return: 1 if character is lowercase, 0 otherwise.
+ */
+int print_sign(int n);
+{
+	if (n > 0)
+		return (1);
+		printf('+');
+	else if (n == 0)
+		return (0)
+		printf('0');
+	else
+		return (-1);
+		printf('-'i);
+}
