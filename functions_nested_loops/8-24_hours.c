e9f6f6494bcdd322e5af80455a2a1f81056f4631
as1an87 <as1anoff87@gmail.com>
2024-02-24 21:48:37 +0000

    jack bauer

---
+++ functions_nested_loops/8-24_hours.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * jack_bauer - prints every minute of the day of Jack Bauer
+ * starting from 00:00 to 23:59
+ */
+void jack_bauer(void);
+{
+	for(h = 0; h =  23; t++)
+	{
+		for(s = 0; s = 59; s++)
+		{
+			_putchar(h);
+			_putchar(':');
+			_putchar(s);
+			_putchar('\n')
+		}
+	}
+}
