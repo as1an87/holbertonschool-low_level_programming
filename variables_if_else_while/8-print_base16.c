9c1bde885dc5786303609f71fc3b5507763af8e0
as1an87 <asn1anoff87@gmail.com>
2024-02-24 18:41:51 +0000

    hexadecimal

--- variables_if_else_while/8-print_base16.c
+++ variables_if_else_while/8-print_base16.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 /**
- * main - Prints all single digit numbers of base 10 starting from 0 then letter a to f
- *
+ * main - Prints all single digit numbers of base 10 starting from 0
+ * then letter a to f
  * Return: Always 0.
  */
 int main(void)
@@ -11,9 +11,9 @@ int main(void)
 
 	for (number = 0; number < 10; number++)
 
-		putchar((number%10) + '0');
+		putchar((number % 10) + '0');
 
-	for (letter = 'a'; letter <='f'; letter++)
+	for (letter = 'a'; letter <= 'f'; letter++)
 
 		putchar(letter);
 
