36db056e128e471915b2e929735f6f1964975f18
as1an87 <as1anoff87@gmail.com>
2024-02-24 21:59:45 +0000

    jack bauer

--- functions_nested_loops/8-24_hours.c
+++ functions_nested_loops/8-24_hours.c
@@ -7,7 +7,7 @@ void jack_bauer(void)
 {
 	int h, m;
 
-	for(h = 0; h <=  23; t++)
+	for(h = 0; h <=  23; h++)
 	{
 		for(m = 0; m <= 59; m++)
 		{
@@ -15,7 +15,7 @@ void jack_bauer(void)
 			_putchar((h % 10) + '0');
 			_putchar(':');
 			_putchar((m / 10) + '0');
-			_putchar((m % 10  + '0');
+			_putchar((m % 10)  + '0');
 			_putchar('\n');
 		}
 	}
