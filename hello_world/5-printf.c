776fe475d1b0571e51e150e30a5fc80f5bd0eee7
as1an87 <asn1anoff87@gmail.com>
2024-02-24 12:41:28 +0000

    hello printf

--- hello_world/5-printf.c
+++ hello_world/5-printf.c
@@ -1,7 +1,9 @@
 #include <stdio.h>
 /**
- * main - Prints "Programming is like building a multilingual
- *                puzzle, followed by a new line.
+ * main - Prints Write a C program that prints exactly with proper grammar,
+ * 		 but the outcome is a piece of art,
+ * 		 followed by a new line.
+ *                
  *
  * Return: Always 0.
  */
