bc2986e941280b1ca8b6eb8cb86377d88eae8c29
as1an87 <as1anoff87@gmail.com>
2024-03-01 20:14:14 +0000

    numbers

---
+++ more_functions_nested_loops/3-print_number.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * print_numbers - print numbers
+ * @i: numbers
+ */
+void print_numbers(void)
+{
+	for (int i = 0; i <= 9; i++)
+	{
+		_putchar(i + '0');
+	}
+	_putchar ("\n");
+	return (0);
+}
