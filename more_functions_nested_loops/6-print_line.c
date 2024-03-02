816facbaaf7c5b5857736ad89913de17071a91c7
as1an87 <as1anoff87@gmail.com>
2024-03-02 15:21:51 +0000

    _

---
+++ more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * void print_line - print n time _
+ * Return :0
+ */
+void print_line(int n)
+{
+	int n;
+	int i;
+
+	while (i < n)
+		_putchar ("_");
+	_putchar ('\n');
+}
