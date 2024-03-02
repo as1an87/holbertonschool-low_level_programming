ab31b6c941792b6dd18a80ae88d46323aaddd719
as1an87 <as1anoff87@gmail.com>
2024-03-02 14:55:24 +0000

    number 1 to 14

--- more_functions_nested_loops/5-more_numbers.c
+++ more_functions_nested_loops/5-more_numbers.c
@@ -5,11 +5,12 @@
  */
 void more_numbers(void)
 {
-	int i,j;
+	char i;
+	char j;
 
-	for (i = 0; i <= 10; i++)
+	for (i = '0'; i <= '10'; i++)
 	{
-		for (j = 0; j <= 14; j++)
+		for (j = '0'; j <= '14'; j++)
 		{
 			_putchar (j);
 		}
