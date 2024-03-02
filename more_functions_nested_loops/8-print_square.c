b719ef348f9d4e9dbeb9a3dc6dfa946b09675450
as1an87 <as1anoff87@gmail.com>
2024-03-02 17:21:04 +0000

    square

---
+++ more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ *print_square - function that prints square
+ *@size: size of square
+ *
+ */
+void print_square(int size)
+{
+	int l,c;
+
+	for (l = 0; l <= size; l++)
+	{
+		for (s = 0; s <= size; c++)
+		{
+			_putchar(35);
+		}
+	}
+}
