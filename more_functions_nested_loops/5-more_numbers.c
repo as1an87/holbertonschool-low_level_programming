04709e7b3fb9a5fa4330f8dbe2b2fe7b220fa8b0
as1an87 <as1anoff87@gmail.com>
2024-03-02 15:13:57 +0000

    number 1 to 14

--- more_functions_nested_loops/5-more_numbers.c
+++ more_functions_nested_loops/5-more_numbers.c
@@ -13,10 +13,10 @@ void more_numbers(void)
 		{
 			if (y > 9)
 			{
-				_putchar((y / 10) + '0');
+				_putchar((j / 10) + '0');
 			}
 
-			_putchar((y % 10) + '0');
+			_putchar((j % 10) + '0');
 		}
 		}
 		_putchar('\n');
