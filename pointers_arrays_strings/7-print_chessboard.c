ed15a5d8da49ddfa5cf7dcade49d84d5fcb91994
as1an87 <as1anoff87@gmail.com>
2024-03-05 13:14:20 +0000

    chessboard

--- pointers_arrays_strings/7-print_chessboard.c
+++ pointers_arrays_strings/7-print_chessboard.c
@@ -10,7 +10,10 @@ void print_chessboard(char (*a)[8])
 	int column;
 
 	for (row = 0; row < 8; row++)
+	{
 		for (column = 0; column < 8; column++)
+		{
 			_putchar(a[row][column]);
+		}
 		_putchar('\n');
-}
+}	}
