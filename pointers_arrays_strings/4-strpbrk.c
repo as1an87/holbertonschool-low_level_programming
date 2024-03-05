7f5b044e22c77ad8984d8086b11b17ba0a97fb36
as1an87 <as1anoff87@gmail.com>
2024-03-05 12:47:34 +0000

    strbrk

--- pointers_arrays_strings/4-strpbrk.c
+++ pointers_arrays_strings/4-strpbrk.c
@@ -1,8 +1,8 @@
 #include "main.h"
 
 /**
- * _strpbrk -  The _strpbrk() function locates the first occurrence in the string s
- *  of any of the bytes in the string accept
+ * _strpbrk -  The _strpbrk() function locates the first occurrence in the 
+ * string s of any of the bytes in the string accept
  * @s: stirng
  * @accept: character
  * Return: null
