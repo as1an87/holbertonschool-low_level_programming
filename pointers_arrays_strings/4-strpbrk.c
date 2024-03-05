ca1b1310ba354cf1f868ec1398cafee5fffda663
as1an87 <as1anoff87@gmail.com>
2024-03-05 12:44:06 +0000

    strbrk

---
+++ pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ *  The _strpbrk() function locates the first occurrence in the string s 
+ *  of any of the bytes in the string accept 
+ * @s: stirng
+ * @accept: character
+ * Return: null
+ */
+char *_strpbrk(char *s, char *accept)
+{
+	int i;
+
+	while (*s)
+	{
+		for (i =0; accept[i]; i++)
+		{
+			if (accept[i] == *s)
+				return (s);
+		}
+		s++;
+	}
+	return ('\0');
+}
+	
