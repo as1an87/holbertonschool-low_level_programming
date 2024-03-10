d1a6796609dc249ab722773fa14c6a1311f1a829
as1an87 <as1anof87@gmail.com>
2024-03-10 17:54:11 +0000

    power

---
+++ recursion/4-pow_recursion.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * _pow_recursion - pow recursion
+ * @x: x raised
+ * @y: to the power of y
+ * Return: pow recursion
+ */
+int _pow_recursion(int x, int y)
+{
+	if (y < 0)
+		return (-1);
+	if (y == 0)
+		return (1)
+	return (x * _pow_recursion(x, y - 1));
+}
