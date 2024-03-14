2e19896fcb232003d2c3bd65bf86aba73b3cc571
as1an87 <as1anoff87@gmail.com>
2024-03-14 11:31:20 +0000

    malloc

--- malloc_free/0-create_array.c
+++ malloc_free/0-create_array.c
@@ -1,5 +1,11 @@
 #include "main.h"
 #include <stdlib.h>
+/**
+ * create_array - create array
+ * @size: size of array
+ * @c: character
+ * Return: null or a
+ */
 
 char *create_array(unsigned int size, char c)
 {
