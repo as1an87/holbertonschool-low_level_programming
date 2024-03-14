f231c64b37d4a8d8fbc18b6be7c33ba5a918ed1f
as1an87 <as1anoff87@gmail.com>
2024-03-14 12:26:17 +0000

    2

---
+++ malloc_free/2-str_concat.c
@@ -0,0 +1,36 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ * str_concat - strconcay
+ * @s1: char
+ * @s2: char
+ */
+char *str_concat(char *s1, char *s2)
+{
+	char *a;
+	int i, j, c, d;
+	
+	if (s1 == NULL)
+		s1 ="";
+	if (s2 == NULL)
+		s2 = "";
+
+	for (i; s1[i] != '\n';i++)
+		;
+	for (j; s2[j] != '\n'; j++)
+		;
+
+	a = malloc((i * sizeof(*s1)) + j * sizeof(*s2) + 1);
+	if (a == NULL)
+		return (NULL);
+
+	for (c = 0, d = 0; c < (i + j + 1); c++)
+	{
+		if (c < i)
+			a[c] = s1[c];
+		else 
+			a[c] = s2[c];
+	}
+
+	return (a);
+}
