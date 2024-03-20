3d6d4d2d0a9225f7d423bb0950888186792cf5cc
as1an87 <as1anoff87@gmail.com>
2024-03-20 19:43:45 +0000

    init

--- structures_typedef/1-init_dog.c
+++ structures_typedef/1-init_dog.c
@@ -1,5 +1,13 @@
 #include "dog.h"
-void init_dog(struct dog *d, char *name, float age, char *owner){
+/**
+ * init_dog -  a function that initialize a variable of type struct dog
+ * @d: struct
+ * @name: name
+ * @age: age
+ * @owner: owner
+ */
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
 	if (d)
 	{
 	d->name = name;
