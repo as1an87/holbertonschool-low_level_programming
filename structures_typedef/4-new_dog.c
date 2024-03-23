05ae394b970ff650ad6563ca68aa6150f355ebe9
as1anoff87@gmail.com <as1anoff87@gmail.com>
2024-03-23 17:25:20 +0000

    newdog

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -9,7 +9,7 @@
  *
  * Return: pointer to new dog
  */
-dog_t *new_dog(char *name, float age, char *owner)
+dog_t *new_dog(char *name, float age, char *owner);
 {
 	unsigned int nl, ol, i;
 	dog_t *dog;
