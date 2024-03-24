a4fb44a9a16f9eec5de75261e15eb7e8581e1241
as1an87 <as1anoff87@gmail.com>
2024-03-24 17:03:33 +0000

    calculator

--- function_pointers/3-main.c
+++ function_pointers/3-main.c
@@ -1,27 +1,30 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "3-calc.h"
-
 int main(int argc, char *argv[])
 {
+	int num1, num2, result;
+	char *operator;
+	int (*operation)(int, int);
+
 	if (argc != 4)
 	{
-		printf("Error\n");
-		return (98);
+	printf("Error\n");
+	return (98);
 	}
 
-	int num1 = atoi(argv[1]);
-	int num2 = atoi(argv[3]);
-	int *operator = argv[2];
-	int result;
-
-	int (*operation)(int, int) = get_op_func(operator);
+	num1 = atoi(argv[1]);
+	operator = argv[2];
+	num2 = atoi(argv[3]);
+       
+	operation = get_op_func(operator);
 
 	if (operation == NULL)
 	{
-		printf("Error\n");
-		return (99);
+	printf("Error\n");
+	return (99);
 	}
+
 	result = operation(num1, num2);
 	printf("%d\n", result);
 
