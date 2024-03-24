09c6bb7fae58469361feef477c711fa34fb7b131
as1an87 <as1anoff87@gmail.com>
2024-03-24 13:41:06 +0000

    print all

---
+++ variadic_functions/3-print_all.c
@@ -0,0 +1,49 @@
+#include "variadic_functions.h"
+#include <stdio.h>
+#include <stdarg.h>
+/**
+ * print_all - function that prints all
+ * @format: format
+ *
+ */
+
+void print_all(const char * const format, ...)
+{
+	va_list arg;
+	va_start(args, format);
+
+	char *separator = "";
+	int i = 0;
+	char c;
+	char *s;
+	float f;
+
+	while (format && format[i])
+	{
+		switch (format[i])
+		{
+			case 'c':
+				c = va_arg(args, int);
+				printf("%s%c", separator,c);
+				break;
+			case 'i':
+				printf(%s%d, separator, va_arg(args, int));
+				break;
+			case 'f':
+				printf(%s%f, separator, va_arg(args, double));
+				break;
+			case 's':
+				if (s == NULL)
+					printf("%s(nil)", separator);
+				else
+					printf("%s%s", separator, s);
+				break;
+			default :
+				break;
+		}
+		separator = ", ";
+		i++;
+	}
+	va_end(args);
+	printf("\n");
+}
