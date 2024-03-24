a261687db750286309cc9334edbd630b95d30ed5
as1an87 <as1anoff87@gmail.com>
2024-03-24 12:46:10 +0000

    printnumber

--- variadic_functions/1-print_numbers.c
+++ variadic_functions/1-print_numbers.c
@@ -9,10 +9,11 @@
 void print_numbers(const char *separator, const unsigned int n, ...)
 {
 	va_list args;
+	unsigned int i;
 
 	va_start(args, n);
 
-	for (unsigned int i = 0; i < n; i++)
+	for (i = 0; i < n; i++)
 	{
 		printf("%d", va_arg(args, int));
 		if (separator != NULL && i < n - 1)
