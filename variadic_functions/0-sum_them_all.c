8333a2093c7cd6358ccad5148b0e33e94fcdb473
as1an87 <as1anoff87@gmail.com>
2024-03-24 09:57:18 +0000

    variadicfunctions

---
+++ variadic_functions/0-sum_them_all.c
@@ -0,0 +1,20 @@
+#include "variadic_functions.h"
+/**
+ *sum_them_all - sum of all its parameters
+ *@n: unsigned int
+ *Return: sum
+ */
+int sum_them_all(const unsigned int n, ...)
+{
+	unsigned int c, sum = 0;
+	va_list ar;
+
+	if (n == 0)
+		return (0);
+
+	va_start(ar, n);
+	for (c = 0; c < n; c++)
+		sum += va_arg(ar, int);
+	va_end;
+	return (sum);
+}
